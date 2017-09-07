9f6175ff5c2aeae51014ebee837ffc3eb0db14e0
Adam Ortyl <adam.ortyl@gmail.com>
2017-09-07 00:43:59 -0400

    Buliding Generator Digest.

--- dev/js/containers/buildings.container.js
+++ dev/js/containers/buildings.container.js
@@ -3,6 +3,22 @@ import {connect} from 'react-redux';
 
 
 class Buildings extends Component {
+    constructor(props, context) {
+        super(props, context);
+
+        this._generatorDigest = () => {
+            //TODO - Cycle through generators and if they can handle the cost, then generate whatever they generate.
+            //TODO - Question: Should there be a single GENERATOR_DIGEST action with a unique payload? or a unique action per building?
+        }
+    }
+
+    componentDidMount() {
+        this.countdown = setInterval(this._generatorDigest, 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.countdown);
+    }
 
     render() {
         const generatorBuildings = Object.keys(this.props.buildings.generator).map((building) => {
@@ -13,7 +29,7 @@ class Buildings extends Component {
 
         return (
             <div>
-                <h4>buildings:</h4>
+                <h4>Buildings:</h4>
                 <ul>{generatorBuildings}</ul>
             </div>
         );
