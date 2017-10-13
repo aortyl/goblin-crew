2face88de7d1209e9c218173a641aeb1451ac4a3
aortyl <adam.ortyl@gmail.com>
2017-10-13 10:17:56 -0400

    Simplest function for creating a goblin crew name

--- dev/js/services/crew.factory.js
+++ dev/js/services/crew.factory.js
@@ -1,1 +1,29 @@
 //TODO - Build a factory that will build out different types of crews.
+
+//TODO - Possible functions:
+/*
+  BuildCrew(type, level),
+  Random GoblinCrew Name
+  Random HumanAdventuring Group name
+  Random Human Village name
+  Random Goblin Village name
+  Random Goblin name
+  Random Human Name s
+  */
+
+function GoblinCrewName(){
+    const adjectives = ['green', 'crooked', 'foul', 'icky', 'contemptuous', 'craggy', 'splintered', 'plump', 'slinky'];
+    const nouns = ['stabbers', 'gobbos', 'stumps', 'rabbits', 'skulkers', 'elbows', 'tooths'];
+
+    let name = getRandom(adjectives) + ' ' + getRandom(nouns);
+    return toTitleCase(name);
+}
+
+function toTitleCase(str)
+{
+    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
+
+function getRandom(array) {
+    return array[Math.floor(Math.random() * array.length)];
+}
